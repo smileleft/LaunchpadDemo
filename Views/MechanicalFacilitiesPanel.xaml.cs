6721dadf90bb30a5a9f0574b63e95aa12bd7da46
smileleft <spyt77@gmail.com>
2026-07-13 15:32:35 +0900

    Add files via upload

---
+++ Views/MechanicalFacilitiesPanel.xaml.cs
@@ -0,0 +1,3 @@
+using System.Windows.Controls;
+namespace LaunchControl.Views;
+public partial class MechanicalFacilitiesPanel : UserControl { public MechanicalFacilitiesPanel() => InitializeComponent(); }
